e370fa0f51c7b8418bb001f115734e8b72c4639c
Dilolabs <contact@dilolabs.fr>
2023-05-30 16:28:43 +0200

    Use CDN for @popperjs/core and @rails/activestorage

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,7 +10,7 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "flowbite" # @1.6.5
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "stimulus-autocomplete" # @3.1.0
 pin "tailwindcss-stimulus-components" # @3.0.4
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.0.5
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.5/app/assets/javascripts/activestorage.esm.js"
