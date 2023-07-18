6cfa9bb0f633f5de9cc5aa56a36a626ff27376bf
Dilolabs <equipe@dilolabs.fr>
2023-07-18 17:04:20 +0200

    Prepare notifications for staging

--- app/notifications/activity_registration_notification.rb
+++ app/notifications/activity_registration_notification.rb
@@ -1,6 +1,5 @@
 class ActivityRegistrationNotification < ApplicationNotification
-  # TODO: Remove development: true
-  deliver_by :ios, format: :ios_format, bundle_identifier: :ios_bundle_identifier, cert_path: :ios_cert_path, key_id: :ios_key_id, team_id: :ios_team_id, pool_size: 5, development: true
+  deliver_by :ios, format: :ios_format, bundle_identifier: :ios_bundle_identifier, cert_path: :ios_cert_path, key_id: :ios_key_id, team_id: :ios_team_id, pool_size: 5, development: Rails.env.development?
   deliver_by :fcm, credentials: :fcm_credentials, format: :format_notification
 
   def format_notification(device_token)
